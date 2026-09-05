29daca49e9e40fc503b887250c5b44ead5309370
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-09-04 21:22:05 -0400

    Add Scratch extension template with logging block

---
+++ examples/template.js
@@ -0,0 +1,24 @@
+    (function(Scratch) {
+  'use strict';
+  class Extension {
+    getInfo() {
+      return {
+        id: "johnMyExtension",
+        name: "My Extension",
+        blocks: [
+          {
+            opcode: 'logToConsole',
+            text: 'log to console',
+            blockType: Scratch.BlockType.COMMAND
+          }
+        ]
+      };
+    }
+
+    logToConsole() {
+      console.log('Hello world!');
+    }
+  }
+
+  Scratch.extensions.register(new Extension());
+})(Scratch);
