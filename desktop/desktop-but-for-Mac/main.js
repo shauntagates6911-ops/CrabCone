e1c9916add5f0bb2b58a46879827484b6491f042
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-09-04 22:47:21 -0400

    Add main.js for Electron app initialization

---
+++ desktop/desktop-but-for-Mac/main.js
@@ -0,0 +1,23 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  });
+
+  // Load the local index.html from the app root
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
